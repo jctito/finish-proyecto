3790df4bb97c33d9230487f93fdb73d14b14b4af
juan carlos <titojuancarlos330@gmail.com>
2023-11-23 03:42:02 -0400

    añadi capas DAD,service,Insertar datos a DB y leer eliminar  actualizar

--- src/CilenteService.java
+++ src/CilenteService.java
@@ -20,12 +20,36 @@ public class CilenteService {
 
     }
     public static void listarCliente(){
+        ClienteDAO.leerClienteDB();
 
     }
     public static void borrarCliente(){
-
+        Scanner juan = new Scanner(System.in);
+        System.out.println("Indique el ID del cliente a borrar: ");
+        int id_cliente = juan.nextInt();
+        ClienteDAO.borrarCliente(id_cliente);
     }
     public static void editarCliente(){
+        Scanner juan = new Scanner(System.in);
+        System.out.println("escribe nuevo nombre ");
+        String nombre =juan.nextLine();
+
+        System.out.println("escribe nuevo email ");
+        String email =juan.nextLine();
+
+        System.out.println("escribe nuevo telefono ");
+        String telefono =juan.nextLine();
+
+        System.out.println("indique el ID del cliente a editar ");
+        int  id_cliente =juan.nextInt();
+
+        Cliente  acliente =new Cliente();
+        acliente.setNombre(nombre);
+        acliente.setEmail(email);
+        acliente.setTelefono(telefono);
+        acliente.setId(id_cliente);
+        ClienteDAO.actualizarCliente(acliente);
+
 
     }
 }
--- src/ClienteDAO.java
+++ src/ClienteDAO.java
@@ -1,33 +1,92 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ClienteDAO {
-    public static void crearClienteDB(Cliente cliente){
+    public static void crearClienteDB(Cliente cliente) {
         Conexion db_connet = new Conexion();
         try (Connection conexion = db_connet.get_conConnection()) {
             PreparedStatement ps = null;
             try {
                 String query = "INSERT INTO `dbtienda`.`cliente` (`nombre`, `email`, `telefono`) VALUES (?, ?, ?)";
                 ps = conexion.prepareStatement(query);
-                ps.setString(1,cliente.getNombre());
-                ps.setString(2,cliente.getEmail());
-                ps.setString(3,cliente.getTelefono());
+                ps.setString(1, cliente.getNombre());
+                ps.setString(2, cliente.getEmail());
+                ps.setString(3, cliente.getTelefono());
                 ps.executeUpdate();
-            } catch (SQLException ex){
+            } catch (SQLException ex) {
                 System.out.println(ex);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e);
         }
     }
-    public static void leerClienteDB(){
+
+    public static void leerClienteDB() {
+        Conexion db_conexion = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try (Connection conexion = db_conexion.get_conConnection()) {
+            String query = "SELECT * FROM cliente";
+            ps = conexion.prepareStatement(query);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id"));
+                System.out.println("Nombre: " + rs.getString("nombre"));
+                System.out.println("Email: " + rs.getString("email"));
+                System.out.println("Telefono: " + rs.getString("telefono"));
+                System.out.println("***");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
 
     }
-    public static  void borrarCliente(int id){
 
-    }
-    public static void actualizarCliente(Cliente cliente){
+    public static void borrarCliente(int id) {
+        Conexion db_connet = new Conexion();
+        try (Connection conexion = db_connet.get_conConnection()) {
+            PreparedStatement ps = null;
+            try {
+                String query = "DELETE FROM cliente WHERE  id=?";
+                ps = conexion.prepareStatement(query);
+                ps.setInt(1, id);
+                ps.executeUpdate();
+                System.out.println("El cliente ah sido borrado");
 
+        } catch (SQLException e) {
+            System.out.println(e);
+            System.out.println("El cliente no se `puede borrar");
+        }
+    }catch(SQLException e)
+
+    {
+        System.out.println(e);
     }
 }
+
+    public static void actualizarCliente(Cliente cliente){
+        Conexion db_connet = new Conexion();
+        try (Connection conexion = db_connet.get_conConnection()) {
+            PreparedStatement ps;
+            try {
+                String query = "UPDATE  cliente SET nombre=?, email=?,telefono`=?, WHERE id=?";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, cliente.getNombre());
+                ps.setString(2, cliente.getEmail());
+                ps.setString(3, cliente.getTelefono());
+                ps.setInt(4, cliente.getId());
+                ps.executeUpdate();
+                System.out.println("Se actualizo correctamente");
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
+    }
+
+
